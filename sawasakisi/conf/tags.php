dd434b466da2622d0361c9f10a47fb8f72dcd1ad
KoOi <evandrokoiti@gmail.com>
2015-03-11 16:15:06 -0300

    [FIX] tags

--- sawasakisi/conf/tags.php
+++ sawasakisi/conf/tags.php
@@ -1,11 +1,24 @@
 <?php
 	#Definições do Sistema
 	date_default_timezone_set('America/Sao_Paulo');
-	define('UrlPadrao' , "http://www.sawasakiimoveis.com/sawasakisi/");
-	
-	#Definições do Banco de Dados
-	define('DB_Host' , "localhost");
-	define('DB_Database' , "sawas543_sawasaki");
-	define('DB_User' , "sawas543_admin");
-	define('DB_Pass' , '$RT%,.;');
+	#Verifica SERVER (web/local)
+    if (strpos($_SERVER['DOCUMENT_ROOT'], 'public_html') !== false) {
+        #WEB
+        define('UrlPadrao' , "http://www.sawasakiimoveis.com/sawasakisi");
+    	
+    	#Definições do Banco de Dados
+    	define('DB_Host' , "localhost");
+    	define('DB_Database' , "sawas543_sawasaki");
+    	define('DB_User' , "sawas543_admin");
+    	define('DB_Pass' , '$RT%,.;');
+    }else{
+        #LOCAL
+        define('UrlPadrao' , "http://localhost/sawasakiimoveis/sawasakisi/");
+    	
+    	#Definições do Banco de Dados
+    	define('DB_Host' , "localhost");
+    	define('DB_Database' , "sawasakisi");
+    	define('DB_User' , "root");
+    	define('DB_Pass' , '');
+    }
 ?>
