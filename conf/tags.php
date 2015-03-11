2365f64391ede81c301bc97d9d98c999c7f1be61
KoOi <evandrokoiti@gmail.com>
2015-03-11 16:17:02 -0300

    [FIX] git ignore

---
+++ conf/tags.php
@@ -0,0 +1,25 @@
+<?php
+	#Definições do Sistema
+	date_default_timezone_set('America/Sao_Paulo');
+    #Verifica SERVER (web/local)
+    if (strpos($_SERVER['DOCUMENT_ROOT'], 'public_html') !== false) {
+        #WEB
+        define('UrlPadrao' , "http://www.sawasakiimoveis.com/");
+    	
+    	#Definições do Banco de Dados
+    	define('DB_Host' , "localhost");
+    	define('DB_Database' , "sawas543_sawasaki");
+    	define('DB_User' , "sawas543_admin");
+    	define('DB_Pass' , '$RT%,.;');
+    }else{
+        #LOCAL
+        define('UrlPadrao' , "http://localhost/sawasakiimoveis/");
+    	
+    	#Definições do Banco de Dados
+    	define('DB_Host' , "localhost");
+    	define('DB_Database' , "sawasakisi");
+    	define('DB_User' , "root");
+    	define('DB_Pass' , '');
+        
+    }
+?>
