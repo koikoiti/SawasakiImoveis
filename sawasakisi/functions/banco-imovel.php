372a0fdaabd343814cda97b5c7c72c026158629f
KoOi <evandrokoiti@gmail.com>
2015-03-17 06:29:33 -0300

    [FIX] mpdf

--- sawasakisi/functions/banco-imovel.php
+++ sawasakisi/functions/banco-imovel.php
@@ -216,7 +216,6 @@
             #Primeira table
             $Auxilio = str_replace('<%REFERENCIA%>', $rs['referencia'], $Auxilio);
             $Auxilio = str_replace('<%ANGARIADOR%>', utf8_encode($rs['angariador']), $Auxilio);
-            $Auxilio = str_replace('<%DATACADASTRO%>', date('d/m/Y - H:i', strtotime($rs['data_cadastro'])), $Auxilio);
             $Auxilio = str_replace('<%ENDERECO%>', utf8_encode($rs['endereco']), $Auxilio);
             $Auxilio = str_replace('<%BAIRRO%>', utf8_encode($rs['bairro']), $Auxilio);
             $Auxilio = str_replace('<%ENTRERUAS%>', utf8_encode($rs['entreruas']), $Auxilio);
@@ -226,9 +225,136 @@
             $Auxilio = str_replace('<%PROPRIETARIO%>', utf8_encode($rs['proprietario']), $Auxilio);
             $Auxilio = str_replace('<%CIDADEESTADO%>', utf8_encode($rs['cidade'] . "/" . $rs['estado']), $Auxilio);
             $Auxilio = str_replace('<%TELEFONE%>', $rs['telefone'], $Auxilio);
+            $Auxilio = str_replace('<%DATACADASTRO%>', date('d/m/Y - H:i', strtotime($rs['data_cadastro'])), $Auxilio);
+            $Auxilio = str_replace('<%CADASTRADOPOR%>', utf8_encode($rs['cadastrado_por']), $Auxilio);
             
             #Segunda table
+            $Auxilio = str_replace('<%AREAUTIL%>', utf8_encode($rs['area_util']), $Auxilio);
+            $Auxilio = str_replace('<%AREATOTAL%>', utf8_encode($rs['area_total']), $Auxilio); 
             $Auxilio = str_replace('<%CATEGORIA%>', utf8_encode($rs['categoria']), $Auxilio);
+            $Auxilio = str_replace('<%CHURRASQUEIRA%>', utf8_encode($rs['churrasqueira']), $Auxilio);
+            $Auxilio = str_replace('<%SALA%>', utf8_encode($rs['sala']), $Auxilio);
+            $Auxilio = str_replace('<%IDADE%>', utf8_encode($rs['idade']), $Auxilio);
+            $Auxilio = str_replace('<%GARAGEM%>', utf8_encode($rs['garagem']), $Auxilio);
+            $Auxilio = str_replace('<%PISO%>', utf8_encode($rs['piso']), $Auxilio);
+            $Auxilio = str_replace('<%ESQUADRIAS%>', utf8_encode($rs['esquadrias']), $Auxilio);
+                #BOOL
+            if($rs['averbada'] != ''){
+                $averbada = "Sim - " . $rs['averbada'];
+            }else{
+                $averbada = "Não";
+            }
+            if($rs['copa'] == 1){
+                $copa = 'Sim';
+            }else{
+                $copa = 'Não';
+            }
+            if($rs['cozinha'] == 1){
+                $cozinha = 'Sim';
+            }else{
+                $cozinha = 'Não';
+            }
+            if($rs['lavabo'] == 1){
+                $lavabo = 'Sim';
+            }else{
+                $lavabo = 'Não';
+            }
+            if($rs['lavanderia'] == 1){
+                $lavanderia = 'Sim';
+            }else{
+                $lavanderia = 'Não';
+            }
+            if($rs['suite'] == 1){
+                $suite = 'Sim';
+            }else{
+                $suite = 'Não';
+            }
+            if($rs['closet'] == 1){
+                $closet = 'Sim';
+            }else{
+                $closet = 'Não';
+            }
+            if($rs['hidromassagem'] == 1){
+                $hidromassagem = 'Sim';
+            }else{
+                $hidromassagem = 'Não';
+            }
+            if($rs['bwc_social'] == 1){
+                $bwc_social = 'Sim';
+            }else{
+                $bwc_social = 'Não';
+            }
+            if($rs['lareira'] == 1){
+                $lareira = 'Sim';
+            }else{
+                $lareira = 'Não';
+            }
+            if($rs['atico'] == 1){
+                $atico = 'Sim';
+            }else{
+                $atico = 'Não';
+            }
+            if($rs['armarios'] == 1){
+                $armarios = 'Sim';
+            }else{
+                $armarios = 'Não';
+            }
+            if($rs['sacada'] == 1){
+                $sacada = 'Sim';
+            }else{
+                $sacada = 'Não';
+            }
+            if($rs['escritorio'] == 1){
+                $escritorio = 'Sim';
+            }else{
+                $escritorio = 'Não';
+            }
+            if($rs['dep_empregada'] == 1){
+                $dep_empregada = 'Sim';
+            }else{
+                $dep_empregada = 'Não';
+            }
+            if($rs['playground'] == 1){
+                $playground = 'Sim';
+            }else{
+                $playground = 'Não';
+            }
+            if($rs['salao_festas'] == 1){
+                $salao_festas = 'Sim';
+            }else{
+                $salao_festas = 'Não';
+            }
+            if($rs['piscina'] == 1){
+                $piscina = 'Sim';
+            }else{
+                $piscina = 'Não';
+            }
+            if($rs['portao_eletronico'] == 1){
+                $portao_eletronico = 'Sim';
+            }else{
+                $portao_eletronico = 'Não';
+            }
+            
+            $Auxilio = str_replace('<%AVERBADA%>', utf8_encode($averbada), $Auxilio);
+            $Auxilio = str_replace('<%COPA%>', utf8_encode($copa), $Auxilio);
+            $Auxilio = str_replace('<%COZINHA%>', utf8_encode($cozinha), $Auxilio);
+            $Auxilio = str_replace('<%LAVABO%>', utf8_encode($lavabo), $Auxilio);
+            $Auxilio = str_replace('<%DORMITORIOS%>', utf8_encode($rs['dormitorios']), $Auxilio);
+            $Auxilio = str_replace('<%SUITE%>', utf8_encode($suite), $Auxilio);
+            $Auxilio = str_replace('<%CLOSET%>', utf8_encode($closet), $Auxilio);
+            $Auxilio = str_replace('<%HIDROMASSAGEM%>', utf8_encode($hidromassagem), $Auxilio);
+            $Auxilio = str_replace('<%BWCSOCIAL%>', utf8_encode($bwc_social), $Auxilio);
+            $Auxilio = str_replace('<%LAVANDERIA%>', utf8_encode($lavanderia), $Auxilio);
+            $Auxilio = str_replace('<%LAREIRA%>', utf8_encode($lareira), $Auxilio);
+            $Auxilio = str_replace('<%ATICO%>', utf8_encode($atico), $Auxilio);
+            $Auxilio = str_replace('<%ARMARIOS%>', utf8_encode($armarios), $Auxilio);
+            $Auxilio = str_replace('<%SACADA%>', utf8_encode($sacada), $Auxilio);
+            $Auxilio = str_replace('<%ESCRITORIO%>', utf8_encode($escritorio), $Auxilio);
+            $Auxilio = str_replace('<%DEPEMPREGADA%>', utf8_encode($dep_empregada), $Auxilio);
+            $Auxilio = str_replace('<%PLAYGROUND%>', utf8_encode($playground), $Auxilio);
+            $Auxilio = str_replace('<%SALAOFESTAS%>', utf8_encode($salao_festas), $Auxilio);
+            $Auxilio = str_replace('<%PISCINA%>', utf8_encode($piscina), $Auxilio);
+            $Auxilio = str_replace('<%PORTAOELETRONICO%>', utf8_encode($portao_eletronico), $Auxilio);
             
             #Terceira table
             $Auxilio = str_replace('<%VALOR%>', number_format($rs['valor'], 2, ',', '.'), $Auxilio);
